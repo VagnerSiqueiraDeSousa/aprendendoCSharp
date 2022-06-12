8c44714df036b47b9774621c2d75298ea769ea4c
VagnerSousa <107362281+VagnerSiqueiraDeSousa@users.noreply.github.com>
2022-06-12 15:47:18 -0300

    Add files via upload
    primeiro projeto

---
+++ 1-PrimeiroProjeto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello word. Primeiro Projeto no visual studio!");
+
+            Console.WriteLine("A execucao acabou. Pressione a tecla para finalizar...");
+            Console.ReadLine();
+
+
+        }
+    }
+}
